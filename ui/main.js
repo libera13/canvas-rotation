a255da33a77ec4675837d14f6670c82cd3ecf3c5
libera13 <emillibera@gmail.com>
2020-11-02 17:49:25 +0100

    add leafsheet

--- ui/main.js
+++ ui/main.js
@@ -8,24 +8,29 @@ const numberOfParticles = 100;
 let particlesArray = [];
 
 const image = new Image();
-image.src = "assets/img/leaf.svg";
+image.src = "assets/img/css_sprites.png";
 
 // Logic
 class Particle {
   constructor() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.size = Math.random() * 200 + 50;
-    this.speed = Math.random() * 11 + 1;
+    this.size = Math.random() * 100 + 50;
+    this.speed = Math.random() * 3 + 0.5;
     this.angle = Math.random() * 360;
     this.spin = Math.random() < 0.5 ? -1 : 1;
-  //  sprite sheet control
+    // sprite sheet control
+    this.frameX = Math.floor(Math.random() * 3);
+    this.frameY = Math.floor(Math.random() * 3);
+    this.spriteSize = 1500/3 ;
   }
   draw() {
     ctx.save();
     ctx.translate(this.x,this.y);
     ctx.rotate(this.angle * Math.PI/360 * this.spin);
-    ctx.drawImage(image, 0 - this.size/2, 0 - this.size/2, this.size, this.size);
+    const currentFrameX = this.frameX * this.spriteSize;
+    const currentFrameY = this.frameY * this.spriteSize;
+    ctx.drawImage(image, currentFrameX, currentFrameY, this.spriteSize, this.spriteSize,  0 - this.size/2,  0 - this.size/2, this.size, this.size );
     ctx.restore();
   }
   update(){
@@ -33,8 +38,8 @@ class Particle {
     if(this.y - this.size > canvas.height){
       this.y = 0 - this.size;
       this.x = Math.random() * canvas.width;
-      this.size = Math.random() * 200 + 50;
-      this.speed = Math.random() * 11 + 1;
+      this.size = Math.random() * 100 + 50;
+      this.speed = Math.random() * 3 + 0.5;
     }
     this.y += this.speed;
   }
